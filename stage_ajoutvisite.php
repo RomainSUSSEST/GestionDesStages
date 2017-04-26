00aa7ddb8198baa12b888caec9bb8873750e938b
tomsmartin <thomas.martin60700@gmail.com>
2017-04-26 16:31:55 +0200

    stage ajout visite values

--- stage_ajoutvisite.php
+++ stage_ajoutvisite.php
@@ -5,15 +5,21 @@
 ?>
 	<div class="container">
 		<div id="content">
-			<h1>ELEVE - CLASSE</h1>
+			<h1>
+				<?php
+					echo $_SESSION['etudiant']." - ".$_SESSION['classe'];
+				?>
+			</h1>
 			<br>
 			<h2>Ajouter une nouvelle visite :</h2>
 			<br>
-			<label>Date de visite :</label>
-			<input />
-			<label>Observations :</label>
-			<textarea></textarea>
-			<button>Valider</button>
+			<form method="POST" action="insert_visiter.php">
+				<label>Date de visite :</label>
+				<input type="text" value="date_visite">
+				<label>Observations :</label>
+				<textarea value="observations"></textarea>
+				<button>Valider</button>
+			</form>
 		</div>
 	</div>
 <?php 
